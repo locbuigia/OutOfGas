9520046ef426056952c25df5f1492911d20534b0
Loc Bui <locbui@uw.edu>
2017-03-01 00:57:49 -0800

    Fix bugs for textview and redesign comment box.

--- app/src/main/java/group5/tcss450/uw/edu/outofgas/DetailActivity.java
+++ app/src/main/java/group5/tcss450/uw/edu/outofgas/DetailActivity.java
@@ -3,7 +3,6 @@ package group5.tcss450.uw.edu.outofgas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
